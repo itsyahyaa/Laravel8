e15378f5a1b88cce05251acbf1b2bd82a1a1be4d
itsyahyaa <yahyamohamed0001@gmail.com>
2021-12-02 14:41:00 +0300

    Custom 404 Page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>Page Not Found</title>
+</head>
+<body>
+    <section style="padding-top:100px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center">
+                    <h1 style="font-size: 162px">404</h1>
+                    <h2>Page Not Found</h2>
+                    <p>We are sorry, the page you requested could not found go back to homepage</p>
+                    <a href="/" class="btn btn-primary">Visit Homepage</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
